a358b6632c9063e6820e2b1cae3728e939a3db8d
Bryan <25674873+bbrown005@users.noreply.github.com>
2021-05-30 13:24:01 -0400

    Added media functionality

--- til/settings.py
+++ til/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/media/"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -41,10 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 
     'feed',
+    'profiles',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'profiles',
+    'sorl.thumbnail',
 
 ]
 
