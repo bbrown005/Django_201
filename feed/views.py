905956a9d44616ca1c30a3088a9087c6a9ecb923
Bryan <25674873+bbrown005@users.noreply.github.com>
2021-06-13 16:59:42 -0400

    added new post functionality with tailwinds modal

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 class HomePage(ListView):
@@ -17,7 +18,18 @@ class PostDetailView(DetailView):
     context_object_name = "post"
 
 
-class CreateNewPost(CreateView):
+class CreateNewPost(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "feed/create.html"
     fields = ['text']
+    success_url = "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
