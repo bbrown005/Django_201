8a0b74331d6e9e9920c683df87e7de2a5c9d18c1
Bryan <25674873+bbrown005@users.noreply.github.com>
2021-06-29 20:26:45 -0400

    Added a profile detail view

--- til/settings.py
+++ til/settings.py
@@ -130,7 +130,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "til/frontend/")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
 SITE_ID = 1
